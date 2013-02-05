f476754f5e761fe03247051673fb165c18126d64
FCW Consulting LLC <FCW@FCW15.local>
2013-02-05 02:07:57 -0600

    extract pixels from JPEG and reconstruct an UIImage with original orientation for display (proof of capability)

--- DrewFace/DrewFaceViewController.m
+++ DrewFace/DrewFaceViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
@@ -28,17 +29,35 @@
     
     // test input image in Documents Directory of iPhone
     NSString *testFileName = @"testimage.jpg";
+    UIImage *testimage;
+    int w;
+    int h;
+    CGFloat scaleDownfactor;
+    int orientation = 0;
     
     // load test input file and display on screen in UIImageView *iv
     NSString *docsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSString *testFilePath = [docsDirectory stringByAppendingPathComponent:testFileName];
     NSFileManager *manager = [NSFileManager defaultManager];
     if ([manager fileExistsAtPath:testFilePath]) {
-        UIImage *testimage = [UIImage imageWithContentsOfFile:testFilePath];
+        testimage = [UIImage imageWithContentsOfFile:testFilePath];
+        NSData *testimageNSData = [NSData dataWithContentsOfFile:testFilePath];
+        CGImageSourceRef source = CGImageSourceCreateWithData((__bridge CFDataRef)testimageNSData, NULL);
+        NSDictionary *metadata = (__bridge NSDictionary *) CGImageSourceCopyPropertiesAtIndex(source,0,NULL);
+        orientation = [[metadata valueForKey:@"Orientation"] integerValue];
+        BOOL DoRotateImage90Degrees = NO;
+        if (orientation>=5) {
+            DoRotateImage90Degrees = YES;
+        }
         iv = [[UIImageView alloc] initWithImage:testimage];
-        CGFloat w = testimage.size.width;
-        CGFloat h = testimage.size.height;
-        CGFloat scaleDownfactor = (w>h)? 320.0/w : 320.0/h;
+        if (DoRotateImage90Degrees) {
+            h = (int)testimage.size.width;
+            w = (int)testimage.size.height;
+        } else {
+            w = (int)testimage.size.width;
+            h = (int)testimage.size.height;
+        }
+        scaleDownfactor = (w>h)? 320.0/w : 320.0/h;
         iv.frame = CGRectMake(0, 100, w*scaleDownfactor, h*scaleDownfactor);
         [self.view addSubview:iv];
     } else {
@@ -48,10 +67,77 @@
     }
     
     
+    // "Bion's been working on a .NET Jpeg->pixel decoder in YCbCr for production use."
+    // Since the production system will have YCbCr available, iOS can be used here to get pixel values
+    CGDataProviderRef myDataProvider = CGImageGetDataProvider(testimage.CGImage);
+    CFDataRef pixelData = CGDataProviderCopyData(myDataProvider);
+    const uint8_t *testimagedata = CFDataGetBytePtr(pixelData);
+    
+    // to test and prove we have a copy of the pixels in BGRA format for the original JPEG
+    uint8_t *mutablebuffer = (uint8_t *)malloc(w*h*4);
+    memcpy(&mutablebuffer[0],testimagedata,w*h*4);
+    for (int i=0; i<h/2; i++) {
+        for (int j=0; j<w; j++) {
+            // do something noticeable with the pixels
+            //*(mutablebuffer + i*w*4 + j*4 + 0) = 0xff;
+        }
+    }
+    CFRelease(pixelData);
+
     
     
+    CGColorSpaceRef colorspaceRef = CGImageGetColorSpace(testimage.CGImage);
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(testimage.CGImage);
+    
+    CGContextRef newContextRef = CGBitmapContextCreate(mutablebuffer, w, h, 8, w*4,colorspaceRef, bitmapInfo);
+    
+    CGImageRef newImageRef = CGBitmapContextCreateImage(newContextRef);
+    
+    
+    UIImage *modifiedImage;
+    if (orientation==6) {
+        modifiedImage = [self imageRotate:[UIImage imageWithCGImage:newImageRef] rotatedby:M_PI/2.0];
+        iv.frame = CGRectMake(0, 100, iv.frame.size.height, iv.frame.size.width);
+    } else {
+        modifiedImage = [self imageRotate:[UIImage imageWithCGImage:newImageRef] rotatedby:0.0];
+    }
+    
+    
+    iv.image = modifiedImage;
+    
+    free(mutablebuffer);
+}
+
+
+- (UIImage *)imageRotate:(UIImage *)origImage rotatedby:(CGFloat)rads
+{
+    // calculate the size of the rotated view's containing box for our drawing space
+    UIView *rotatedViewBox = [[UIView alloc] initWithFrame:CGRectMake(0,0,origImage.size.width, origImage.size.height)];
+    CGAffineTransform t = CGAffineTransformMakeRotation(rads);
+    rotatedViewBox.transform = t;
+    CGSize rotatedSize = rotatedViewBox.frame.size;
+    
+    // Create the bitmap context
+    UIGraphicsBeginImageContext(rotatedSize);
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    
+    // Move the origin to the middle of the image so we will rotate and scale around the center.
+    CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
+    
+    //   // Rotate the image context
+    CGContextRotateCTM(bitmap, rads);
+    
+    // Now, draw the rotated/scaled image into the context
+    CGContextScaleCTM(bitmap, 1.0, -1.0);
+    CGContextDrawImage(bitmap, CGRectMake(-origImage.size.width / 2, -origImage.size.height / 2, origImage.size.width, origImage.size.height), [origImage CGImage]);
+    
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
     
 }
+
+
 
 
 - (void)didReceiveMemoryWarning
