63079effa9369c78dfbc721052a6b5fcc228174e
unknown <bionoren@gmail.com>
2013-04-05 22:44:23 -0700

    distribute directory

---
+++ distribute/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaceDetect;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to detect an image.");
+            Console.ReadLine();
+
+            FaceDetect.GeometryType geometryType = new FaceDetect.GeometryType();
+            geometryType.fileName = "C:\\test.jpg";
+            FaceDetect.FaceDetector.detectFaces(geometryType);
+
+            Console.WriteLine("List value is {0}", geometryType.teethArea.ToString());
+            foreach (Point p in geometryType.teethArea)
+            {
+
+                Console.WriteLine("{0},{1}", p.x,p.y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
