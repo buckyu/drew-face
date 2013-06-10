74df6077d34b8d14c2e32624c80348b6ec8a2e92
drew crawford <bion@drewcrawfordapps.com>
2013-06-10 22:14:15 +0000

    Additional testing of non-jpeg input

--- test/ConsoleApplication2/Program.cs
+++ test/ConsoleApplication2/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApplication2
             Console.ReadLine();
 
             FaceDetect.GeometryType geometryType = new FaceDetect.GeometryType();
-            geometryType.fileName = "C:\\Bears.jpg";
+            geometryType.fileName = "C:\\test.jpg";
             FaceDetect.FaceDetector.detectFaces(geometryType);
 
             if (geometryType.fileName.Length > 0)
@@ -34,6 +34,30 @@ namespace ConsoleApplication2
             else
             {
                 Console.WriteLine("Couldn't find a face and/or mouth");
+                Console.WriteLine("It's also possible that a jpeg image wasn't provided.");
+            }
+
+            geometryType = new FaceDetect.GeometryType();
+            geometryType.fileName = "C:\\test.png";
+            FaceDetect.FaceDetector.detectFaces(geometryType);
+
+            if (geometryType.fileName.Length > 0)
+            {
+                Console.WriteLine("List value is {0}", geometryType.teethArea.ToString());
+                foreach (Point p in geometryType.teethArea)
+                {
+
+                    Console.WriteLine("{0},{1}", p.x, p.y);
+                }
+
+                String mouthImage = "C:\\mouth.jpg";
+                String result = FaceDetect.FaceDetector.stitchFace(geometryType, mouthImage);
+                Console.WriteLine("Result written to {0}", result);
+            }
+            else
+            {
+                Console.WriteLine("Couldn't find a face and/or mouth");
+                Console.WriteLine("It's also possible that a jpeg image wasn't provided.");
             }
             
             Console.ReadLine();
